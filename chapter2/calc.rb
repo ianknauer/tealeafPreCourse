7f2553987805c6af6f97f8bd776162b96e5d3600
ianknauer <ian.knauer@gmail.com>
2013-08-30 17:30:33 -0700

    first half of chapter two calculator exercises

---
+++ chapter2/calc.rb
@@ -0,0 +1,12 @@
+second = 1
+minute = 60
+hour = 60
+day = 24
+year = 365
+decade = 10
+
+hoursInAYear = day * year
+minutesInADecade = ((hour * day) * year) * decade
+
+puts hoursInAYear
+puts minutesInADecade

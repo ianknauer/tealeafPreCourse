8cccc1eac4cca840007dcfa42cbd18edf3ff6e73
ianknauer <ian.knauer@gmail.com>
2013-08-30 22:08:28 -0700

    Finished Second Chapter

--- chapter2/calc.rb
+++ chapter2/calc.rb
@@ -4,9 +4,15 @@ hour = 60
 day = 24
 year = 365
 decade = 10
+myage = 25
+authorsAgeInSeconds = 1160000000
 
 hoursInAYear = day * year
 minutesInADecade = ((hour * day) * year) * decade
+myAgeInSeconds = minute * hour *day *decade * myage
+authorsAge = authorsAgeInSeconds / minute / hour / day / year
 
 puts hoursInAYear
 puts minutesInADecade
+puts myAgeInSeconds
+puts authorsAge

352a7dc97980916edc25e760c189ece377d47f22
ianknauer <ian.knauer@gmail.com>
2013-09-02 09:44:23 -0700

    finished first half of exercise 5.6

---
+++ chapter5/exercise56.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, what\'s your first name?'
+fname = gets.chomp
+puts 'great ' + fname + ', what\'s your middle name?'
+mname = gets.chomp
+puts 'interesting... and what\'s your last name?'
+lname = gets.chomp
+
+puts 'really? Your name is ' + fname + ' ' + mname + ' ' + lname

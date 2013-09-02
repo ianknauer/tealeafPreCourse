8b769d4ff2314e5c5e22ccf17b02084d03670761
ianknauer <ian.knauer@gmail.com>
2013-09-02 09:53:15 -0700

    Finished up chapter 5 along with the exercises in it

--- chapter5/exercise56.rb
+++ chapter5/exercise56.rb
@@ -4,5 +4,10 @@ puts 'great ' + fname + ', what\'s your middle name?'
 mname = gets.chomp
 puts 'interesting... and what\'s your last name?'
 lname = gets.chomp
-
 puts 'really? Your name is ' + fname + ' ' + mname + ' ' + lname
+name = fname + ' ' + mname + ' ' + lname
+puts ''
+puts 'so ' + name + '..., what is your favourite number?'
+fnumber = gets.chomp
+newfnumber = fnumber.to_i + 1
+puts 'you know what they say, bigger is always better... are you sure you don\'t like ' + newfnumber.to_s + ' more?' 

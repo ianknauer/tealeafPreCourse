f9610eaba7e28a983d17e32df0e21dffabdf3c54
ianknauer <ian.knauer@gmail.com>
2013-09-10 14:21:57 -0700

    finished the deaf grandma part of this chapter

--- chapter7/deafgrandma.rb
+++ chapter7/deafgrandma.rb
@@ -1,19 +1,24 @@
-
+bCount = 0
 gDefault = "HUH?! SPEAK UP, SONNY!"
 destroy = "BYE"
 puts "HELLO SONNY HOW ARE YOU?!"
 while true
-text = gets.chomp
-upper = text.upcase
-number = 1930 + rand(21)
-gCaps = "NO, NOT SINCE " + number.to_s
-if text == destroy
-  puts "GOOD BYE YOUNG ONE"
-  break
-elsif text == upper
-  puts gCaps.to_s
-else 
-  puts gDefault
-end
+  text = gets.chomp
+  upper = text.upcase
+  number = 1930 + rand(21)
+  gCaps = "NO, NOT SINCE " + number.to_s.chomp
+    if text == destroy
+      bCount += 1
+    else
+      bCount = 0
+    end
+    break if bCount >= 3
+   
+    response = if text != upper
+      gDefault
+    else 
+      gCaps
+    end
+  puts response
 end
 

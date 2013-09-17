b504693a6a22c2ab04cce53b444d8d83328de4c1
ianknauer <ian.knauer@gmail.com>
2013-09-16 20:25:53 -0700

    first question in chapter nine done

---
+++ chapter9/askmethod.rb
@@ -0,0 +1,30 @@
+def ask question 
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no') 
+      if reply == 'yes'
+        return 'true'
+    else
+        return 'false'
+    end
+    break
+   else
+      puts 'Please answer "yes" or "no".'
+   end 
+end
+answer
+end
+  puts 'Hello, and thank you for...'
+  puts
+  ask 'Do you like eating tacos?'
+  wets_bed = ask 'Do you wet the bed?' 
+  ask 'Do you like eating sopapillas?'
+  puts 'Just a few more questions...'
+  ask 'Do you like drinking horchata?'
+  ask 'Do you like eating flautas?'
+  puts
+  puts 'DEBRIEFING:'
+  puts 'Thank you for...'
+  puts
+  puts wets_bed
